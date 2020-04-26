7b1b7b4cd85694a379a2d3d6f2254156dc6824b0
Kunal Dutta <kdutta@berkeley.edu>
2020-04-26 00:59:23 -0700

    added request to test.html on homepage

--- DIYFitness/views.py
+++ DIYFitness/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-	return HttpResponse("homepage")
+	return render(request, 'test.html')
 
 def test(request):
 	return HttpResponse('blog')
